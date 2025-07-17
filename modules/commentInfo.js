2aff298a6ea61ffccf6085d75da7a9bf6162e49f
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-07-17 18:44:58 +0300

    Created a file commentInfo.js

---
+++ modules/commentInfo.js
@@ -0,0 +1,1 @@
+    const commentInfo = [{name: "Глеб Фокин", text: "Это будет первый комментарий на этой странице", data: "12.02.22 12:18", counter: 3, likeActive: false}, {name: "Варвара Н.", text: "Мне нравится как оформлена эта страница! ❤", data: "13.02.22 19:22", counter: 75, likeActive: true}];
