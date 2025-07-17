05955c937eee66126744d66fa7bbc2d7d0f96303
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-07-17 18:46:51 +0300

    Created a file initListeners.js

---
+++ modules/initListeners.js
@@ -0,0 +1,27 @@
+const initLikeListeners = () => {
+      const likeButtonElements = document.querySelectorAll(".like-button");
+
+      for (const likeButtonElement of likeButtonElements) {
+        likeButtonElement.addEventListener("click", (event) => {
+          event.stopPropagation();
+
+          const index = likeButtonElement.dataset.index;
+          const comment = commentInfo[index];
+
+          comment.counter = comment.likeActive ? comment.counter - 1 : comment.counter + 1;
+          comment.likeActive = !comment.likeActive;
+
+          renderComments();
+        })
+      };
+
+      const commentsElements = document.querySelectorAll(".comment");
+
+      for (const commentsElement of commentsElements) {
+        commentsElement.addEventListener("click", () => {
+          const currentComment = commentInfo[commentsElement.dataset.index]
+
+          commentEl.value = `${currentComment.name}: ${currentComment.text}`
+        })
+      };
+    }
