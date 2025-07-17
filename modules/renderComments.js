11f60f0fb4f9990f17c8b6a11c395459eda735a4
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-07-17 18:50:01 +0300

    Created a file renderComments.js

---
+++ modules/renderComments.js
@@ -0,0 +1,27 @@
+const renderComments = () => {
+      const commentsHTML = commentInfo
+      .map((comment, index) => {
+        return `<li class="comment" data-index="${index}">
+          <div class="comment-header">
+            <div>${comment.name}</div>
+            <div>${comment.data}</div>
+          </div>
+          <div class="comment-body">
+            <div class="comment-text">
+              ${comment.text}
+            </div>
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.counter}</span>
+              <button class="like-button ${comment.likeActive ? "-active-like" : ""}" data-index="${index}"></button>
+            </div>
+          </div>
+        </li>`
+      })
+      .join("");
+
+      listCommentEl.innerHTML = commentsHTML;
+
+      initLikeListeners();
+    }
