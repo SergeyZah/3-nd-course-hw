f427f6496018dd5adedfa26f8bcfd0e5c4845c9c
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-08-04 16:48:03 +0300

    Implemented the method "POST"

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { renderComments } from './modules/renderComments.js'
 import { updateCommentInfo } from './modules/commentInfo.js'
 
-fetch('https://wedev-api.sky.pro/api/v1/:sergey-zaharychev/comments', {
+fetch('https://wedev-api.sky.pro/api/v1/sergei-zaharychev/comments', {
     method: 'GET',
 })
     .then((response) => {
--- modules/renderComments.js
+++ modules/renderComments.js
@@ -3,25 +3,25 @@ import { initLikeListeners } from './initListeners.js'
 import { initCommentListeners } from './initListeners.js'
 
 const listCommentEl = document.querySelector('.comments')
-const likeButtonEl = document.querySelector('.like-button')
 
 export const renderComments = () => {
+  
     const commentsHTML = commentInfo
-        .map((comments, index) => {
+        .map((comment, index) => {
             return `<li class="comment" data-index="${index}">
           <div class="comment-header">
-            <div>${comments.author.name}</div>
-            <div>${comments.date}</div>
+            <div>${comment.author.name}</div>
+            <div>${comment.date}</div>
           </div>
           <div class="comment-body">
             <div class="comment-text">
-              ${comments.text}
+              ${comment.text}
             </div>
           </div>
           <div class="comment-footer">
             <div class="likes">
-              <span class="likes-counter">${comments.likes}</span>
-              <button class="like-button ${comments.isLiked ? '-active-like' : ''}" data-index="${index}"></button>
+              <span class="likes-counter">${comment.likes}</span>
+              <button class="like-button ${comment.isLiked ? '-active-like' : ''}" data-index="${index}"></button>
             </div>
           </div>
         </li>`
